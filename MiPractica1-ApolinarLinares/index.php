f2f32cf68d3fd727fbc8c0b0722c74f9c44862d7
Apolinar Linares <41924872+apolinares@users.noreply.github.com>
2025-07-07 16:49:22 -0400

    iniciando con la practica del Modulo 4 PHP

---
+++ MiPractica1-ApolinarLinares/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicios de PHP</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        .logo {
+            height: 80px;
+        }
+        .container {
+            margin-top: 50px;
+        }
+        </style>
+    </head>
+    <body>
+        <div class="container text-center">
+            <img src="../MiPractica1-ApolinarLinares/img/LogoUnivalle.png" alt="Logo PostGrado Univalle" class="logo mb-3">
+            <h1>DIPLOMADO EN DESARROLLO WEB FULL STACK
+            <br> MODULO IV: FULL STACK PHP</h1>
+            <h3>Bienvenido a los ejercicios prácticos en PHP</h3>
+            <h3>Participante: Apolinar Linares Flores</h3>
+            <div class="list-group">
+                <a href="multiplicacion_rusa.php" class="list-group-item list-group-item-action">
+                    Ejercicio 1: Método de multiplicación Ruso
+                </a>
+                <a href="palindromo.php" class="list-group-item list-group-item-action">
+                    Ejercicio 2: Verificar si un número es palíndromo
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
